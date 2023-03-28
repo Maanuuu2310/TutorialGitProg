387b3ba122a8cb122fb38dd21099aafe3edf34aa
Manuel Gámez <macbookpro@MacBook-Pro-de-Manuel.local>
2023-03-28 10:44:11 +0100

    uwu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println(" introduce un papu numero");
+        String num = scanner.nextLine();
+        System.out.println("Hello, World " + num + " !" );
+        scanner.close();
     }
 }
